35005c46f7bfc4a5f5b8e7beaf8326fbdffc678f
UDemis <support@demis.ru>
2017-05-15 17:31:44 +0300

    2. Selection sort, page 57

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-var mas = [];
-// for (var i = 0; i < 100; i = i+2) {
-// 	mas.push(i);
-// }
-mas = [1,3,5,7,9];
-let secret = 7;
-console.log(mas);
-console.log(find_binary(secret, mas));
-function find_binary(secret, mas){
-	let item = mas;
-	let min = 0;
-	let max = item.length - 1 ;
-	let count = 0;
-	while (min <= max) {
-		let mid = Math.round(( max + min ) / 2);
-		console.log( count, item[min], item[max], item[mid], secret );
-		count++;
-		if ( secret == item[mid] ) {
-			return secret;
-		}
-		if (secret > item[mid]){
-			min = mid + 1;
-		} else {
-			max = mid - 1;
-		}
-		
+function findSmallest( arr ){
+	let smallest = arr[0];
+	let small_index = 0;
+	for (var i = 0; i < arr.length; i++) {
+		if ( arr[i] < smallest){
+			smallest = arr[i];
+			small_index = i;
+		} 
 	}
-	return null;
+	return small_index;
 }
+function selectionSort( arr ){
+	let toSort = arr;
+	let newArr = [];
+	while (toSort.length > 0){
+		let smallest = findSmallest(toSort);
+		newArr.push( toSort[ smallest ] );
+		toSort.splice( smallest, 1 );
+	}
+	return newArr;
+}
+console.log(selectionSort([5, 3, 6, 2, 10]));
--- src/ready.js
+++ src/ready.js
@@ -1,4 +1,10 @@
-function find_binary(secret, mas){
+/**
+ * @param  {Number} secret, choose your secret Number
+ * @param  {Array} mas, in what Array we're going to find
+ * @return {Number}, if secret number exists in Array, or
+ * 					 Null, if not
+ */
+function find_binary (secret, mas) {
 	let item = mas;
 	let min = 0;
 	let max = item.length - 1 ;
@@ -18,4 +24,34 @@ function find_binary(secret, mas){
 		
 	}
 	return null;
+}
+/**
+ * @param  {Array} arr, in what Array 
+ * 					we going to find smallest Number
+ * @return {Number} index of smallest number
+ */
+function findSmallest( arr ){
+	let smallest = arr[0];
+	let small_index = 0;
+	for (var i = 0; i < arr.length; i++) {
+		if ( arr[i] < smallest){
+			smallest = arr[i];
+			small_index = i;
+		} 
+	}
+	return small_index;
+}
+/**
+ * @param  {Array} Array to sort
+ * @return {Array} new sorted Array
+ */
+function selectionSort( arr ){
+	let toSort = arr;
+	let newArr = [];
+	while (toSort.length > 0){
+		let smallest = findSmallest(toSort);
+		newArr.push( toSort[ smallest ] );
+		toSort.splice( smallest, 1 );
+	}
+	return newArr;
 }
