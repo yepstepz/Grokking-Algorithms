4d219be46395b4a8ac1858a8765d9d38d005ecfc
yepstepz <prettyoptimist@gmail.com>
2017-05-17 01:20:06 +0300

    TODO: BFS alg

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-var user = {
-  name: "Вася",
-  age: 25,
-  roles: {
-    isAdmin: false,
-    isEditor: true
+let followers = [
+        {
+            Dan : {},
+            Lila: {}
+        },
+        {
+            Nasty: {},
+            Alice: {},
+            Sergey: {}
+        },
+        {
+            Lila:{},
+            Katy: {
+                mango: 'mango'
+            }
+        }
+]
+function is_person_seller( obj ){
+  console.log(obj);
+  if ( 'mango' in obj && obj.mango != '' ){
+    return true;
   }
-};
+  return false;
+}
+function isSeller( obj ){
+    var queue = [];
+    queue.push(obj[0]);
+    for ( i = 0; i < queue.length; i++){
+          if (is_person_seller( queue[i] )){
+            return "Here's come DATTT BOIIII";
+          } else {
+              console.log( ...queue[i] );
+              queue.push(...queue[i]);
+          }
+
+        }
+
+}
+isSeller(followers);
