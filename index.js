f43435eeb12e2094ae438ce386409cc5cb9806b8
yepstepz <prettyoptimist@gmail.com>
2017-05-19 02:11:38 +0300

    Dijkstra's algorithm added. TODO: tests for other graphs. page 181

--- index.js
+++ index.js
@@ -1,64 +1,108 @@
-let graph = {
-        cost: 0,
-        A : {
-            checked: false,
-            cost: 6,
-            finish : {
-                checked: false,
-                end: 1,
-                cost: 1
-            }
-        },
-        B : {
-            checked: false,
-            cost: 2,
-            A : {
-                checked: false,
-                cost: 3,
-                finish : {
-                    checked: false,
-                    end: 1,
-                    cost: 1
-                }
-            },
-            finish : {
-                checked: false,
-                end: 1,
-                cost: 5
+// let graph = {
+//     start: {
+//         A: 6,
+//         B: 2,
+//     },
+//     A: {
+//         end: 1
+//     },
+//     B: {
+//         A: 3,
+//         end: 5
+//     },
+//     end: {}
+// }
+// let parents = {
+//     A: 'start',
+//     B: 'start',
+//     end: null
+// }
+// let costs = {
+//     A: 6,
+//     B: 2,
+//     end: Infinity
+// }
+// Набор два:
+// let graph = {
+//     start : {
+//         C: 5,
+//         A: 2
+//     },
+//     A : {
+//         C: 8,
+//         B: 7
+//     },
+//     B : {
+//         end: 1
+//     },
+//     C : {
+//         D: 4,
+//         B: 2
+//     },
+//     D : {
+//         end: 3,
+//         B: 6
+//     },
+//     end : {}
+// }
+// let parents = {
+//     C: 'start',
+//     A: 'start',
+//     B: null,
+//     D: null,
+//     end: null
+// }
+// let costs = {
+//     C: 5,
+//     A: 2,
+//     B: Infinity,
+//     D: Infinity,
+//     end: Infinity
+// }
+let checked = [];
+function in_array(item, array) {
+    if (array.length) {
+        for (let i = 0; i < array.length; i++) {
+            if (!item.indexOf(array[i])) {
+                return false;
             }
         }
-}
-/**
- * Checks if Object contains 'end'
- * @param obj Object
- * @returns Bool with result
- */
-function is_finished(obj) {
-    if ( !!obj['end'] ) {
-        return true;
     }
-    return false;
+    return true;
 }
-
+function find_lowest_cost_node(costs) {
+    let lowest_cost = Infinity;
+    let lowest_cost_node = null;
+    for (node in costs) {
+        if (in_array(node, checked) && costs[node] < lowest_cost) {
+            lowest_cost = costs[node];
+            lowest_cost_node = node;
+        }
+    }
+    return lowest_cost_node;
+}
 /**
  * BFS
  * @param root Object with other objects
  * @returns String with results of searching;
  */
 function cheap_way(root) {
-    var queue = root;
-    while (Object.keys(queue).length != 0) {
-        let costs = [];
-        for ( item in queue ){
-                let way;
-                let min = item.cost;
-                if ( !item.checked && !!item.indexOf('cost') ){
-                    costs.push(queue[item]);
-                }
-                console.log(costs);
+    let node = find_lowest_cost_node(costs);
+    while (node != null) {
+        console.log(costs, parents, checked);
+        let cost = costs[node];
+        let neighbors = Object.assign(graph[node]);
+        for (neighbor in neighbors) {
+            let new_cost = cost + neighbors[neighbor];
+            if (costs[neighbor] > new_cost) {
+                costs[neighbor] = new_cost;
+                parents[neighbor] = node;
+            }
         }
-        return false;
+        checked.push(node);
+        node = find_lowest_cost_node(costs);
     }
+    return false;
 
 }
 console.log(cheap_way(graph));
